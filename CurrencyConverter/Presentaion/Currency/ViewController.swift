b212a2ad82c8fc0e2b84c5adc88b764177feb17e
Roy <shuwj81@daum.net>
2025-09-29 12:27:28 +0900

    init: 프로젝트 설정

---
+++ CurrencyConverter/Presentaion/Currency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Wonji Suh  on 9/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .white
+  }
+
+
+}
+
