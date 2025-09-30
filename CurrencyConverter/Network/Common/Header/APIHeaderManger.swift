145964e5f062b4138439d94b0dfd9bff76767292
Roy <shuwj81@daum.net>
2025-09-30 10:20:50 +0900

    ✨[feat]: 네트워크 common 구현

---
+++ CurrencyConverter/Network/Common/Header/APIHeaderManger.swift
@@ -0,0 +1,12 @@
+//
+//  APIHeaderManger.swift
+//  CurrencyConverter
+//
+//  Created by Wonji Suh  on 9/29/25.
+//
+
+import Foundation
+
+public enum APIHeaderManger {
+  static let contentType: String = "application/json"
+}
